0b5bbb4eb95a39472ba277416220117ca43ff566
dienamo <dienamoustapha@gmail.com>
2020-01-18 12:47:45 +0100

    commit du 1801

--- models/order.js
+++ models/order.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema ({
+const orderSchema = new Schema ([{
   store : {type : String},
   product : {type : String},
-})
+}])
 
 const Order = mongoose.model('Order' , orderSchema);
 
